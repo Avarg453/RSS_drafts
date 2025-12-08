52adb19724b6d7e0e88ea6da1a8a930fb90ac4a1
Alex V <avarg453@gmail.com>
2025-12-08 18:08:20 -0500

    adding neo4j stuff, needs set up outside maybe orchestrate with shell

---
+++ neo4j_chunk.py
@@ -0,0 +1,4 @@
+from neo4j import GraphDatabase
+# add in a bunch of stuff for connecting to the database locally which i guess needs teh service to be started
+
+# can teh service run idefinitely on my computer? how do i ensure it runs regularly?
