efeee24545e5fdd6f8dcacbf4ee83b273ae0ecd8
Alex V <avarg453@gmail.com>
2025-12-14 16:12:56 -0500

    recent changes to neo4j

--- neo4j_chunk.py
+++ neo4j_chunk.py
@@ -1,4 +1,18 @@
 from neo4j import GraphDatabase
+import os
+from dotenv import load_dotenv
+load_dotenv()
+# URI examples: "neo4j://localhost", "neo4j+s://xxx.databases.neo4j.io"
+URI = os.getenv("NEO4J_URI")
+USER = os.getenv("NEO4J_USER")
+PASSWORD = os.getenv("NEO4J_PASSWORD")
+AUTH = (USER, PASSWORD)
 # add in a bunch of stuff for connecting to the database locally which i guess needs teh service to be started
 
 # can teh service run idefinitely on my computer? how do i ensure it runs regularly?
+
+
+
+# with GraphDatabase.driver(URI, auth=AUTH) as driver:
+#     driver.verify_connectivity()
+#     print("Connection established.")
