22eff3353ec39b2804311807125ec7a8c3fe757b
Alex Vargas <alexvargas@dce35222-c02e-447d-b94a-8798cf18d668.attlocal.net>
2025-12-14 21:44:29 -0500

    got rss parser and json dump working, will need to sort out neo4j

--- feedparser_script.py
+++ feedparser_script.py
@@ -1,51 +1,65 @@
 import feedparser
-import pprint
-
-# rss_feed=feedparser.parse("https://www.dropsitenews.com/feed")
-rss_feed=feedparser.parse("https://www.thebignewsletter.com/feed")
-# rss_feed.keys
-# dict_keys(['bozo', 'entries', 'feed', 'headers', 'etag', 'href', 'status', 'encoding', 'version', 'namespaces'])
-
-# rss_feed.entries[0].keys()
-# dict_keys(['title', 'title_detail', 'summary', 'summary_detail', 'links', 'link', 'id', 'guidislink', 'authors', 'author', 'author_detail', 'published', 'published_parsed', 'content'])
-
-# rss_feed.entries[0]['title_detail']
-
-# rss_feed.entries[0]['summary_detail']
-
-# rss_feed.entries[0]['link'] # link to article
-
-# rss_feed.entries[0]['published_parsed'] #all of these are ints
-#  time.struct_time(tm_year, tm_mon, tm_mday, tm_hour, tm_min, tm_sec, tm_wday, tm_yday, tm_isdst)
-
-# rss_feed.entries[0]['id'] # this might always just be the same as the link
-
-for key in rss_feed:
-    if type(rss_feed[key]) is dict:
-        for subkey in rss_feed[key]:
-            print(f"for {key}:{subkey}:{len(rss_feed[key][subkey])}")
-    elif type(rss_feed[key]) is list:
-        print(f"for {key}:{len(rss_feed[key])}")
-    else:
-        print(f"{key} is type: {type(rss_feed[key])}")
-
-# print(rss_feed.entries[0]['title_detail'])
-
-# print(rss_feed.entries[0]['summary_detail'])
-
-# print(rss_feed.entries[0]['link'])
-
-# print(rss_feed.entries[0]['published_parsed'])
-
-# print(rss_feed.entries[0]['id'])
-
-
-
-test_article=rss_feed.entries[0]['content'][0]['value']
-
-
 import html2text
+import json
+from urllib.parse import urlparse
+import os
 
 h = html2text.HTML2Text()
 
-print(h.handle(test_article))
+# list of rss feeds to parse
+rss_feed_list=[
+    "https://boltsmag.org/feed/",
+    "https://www.thebignewsletter.com/feed",
+    "https://www.counterpunch.org/feed/",
+    "https://jacobinlat.com/feed/", 
+    "https://www.kenklippenstein.com/feed", 
+    "https://zeteo.com/feed", 
+    "https://www.leefang.com/feed", 
+    "https://www.propublica.org/feed", 
+    "https://mexicosolidarity.com/feed/", 
+    "https://www.compactmag.com/feed/"
+]
+
+os.makedirs('json_dump', exist_ok=True)
+
+for feed in rss_feed_list:
+    rss_feed=feedparser.parse(feed)
+    for entry in rss_feed.entries:
+        entry_data={}
+
+
+        datelist=[]
+        datetuple=entry['published_parsed'][0:6]
+        for elem in datetuple:
+            datelist.append(str(elem))
+        entrydatecomp='-'.join(datelist) 
+        entrylinkcomp=max(urlparse(entry['link']).netloc.split('.'), key=len)
+        # line above is taking the main domain chunk and splitting on the . presumably 
+        # after www and before com/org/net and then sorting that list based on length
+        # since domain will most likely just be the max length thing anyways
+        
+        uniq_ID=entrylinkcomp+'-'+entrydatecomp
+
+        entry_data['uID']=uniq_ID
+
+        entry_data['link']=entry['link']
+
+        entry_data['title']=entry['title']
+
+        entry_data['author']=entry['author']
+
+        entry_data['content']=h.handle(entry['content'][0]['value'])
+
+        json_data = json.dumps(entry_data, indent=4)
+
+        with open(f"./json_dump/{uniq_ID}.json", "w") as f:
+            f.write(json_data)
+
+# for key in rss_feed:
+#     if type(rss_feed[key]) is dict:
+#         for subkey in rss_feed[key]:
+#             print(f"for {key}:{subkey}:{len(rss_feed[key][subkey])}")
+#     elif type(rss_feed[key]) is list:
+#         print(f"for {key}:{len(rss_feed[key])}")
+#     else:
+#         print(f"{key} is type: {type(rss_feed[key])}")
